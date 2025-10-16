e54e723c7bcffa26f08488bd5d7cd7fce9e59d10
Kashi Das Mongre <161732160+kashidasmongre@users.noreply.github.com>
2025-10-16 23:08:41 +0530

    Update entrypoint.sh to preserve environment variables
    Modified entrypoint to start n8n with environment variables.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ chown -R node:node /home/node/.n8n
 
 echo "Permissions fixed."
 
-# Ab, suraksha ke liye 'node' user bankar n8n ko start karo.
-# 'tini' aur 'su-exec' is kaam ko sahi se karte hain.
-exec tini -- su-exec node n8n start
+# Ab, 'node' user bankar n8n ko start karo,
+# lekin saare environment variables ko saath lekar jao.
+exec tini -- su-exec node sh -c 'exec n8n start'
